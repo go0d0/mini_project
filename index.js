6291a8fe7252ab29d61c6bf5df2b261b62cb7797
zacky <zackyfirdaus934@gmail.com>
2025-05-24 05:01:02 -0400

    selesai

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const aksi = async () => {
   const response = await fetch ("https://randomuser.me/api/");
   if (!response) return new Error("gagal terhubung");
   
-  const data = await response.json();
-  const {} =
+  const {results} = await response.json();
+  const [data] =results;
+
+  const {name:{first,last}, email, location:{city,country}, picture:{large}} = data;
+
+  fungsi ({first,last,email,city,country,large});
   
   } catch (err){
     document.getElementById("ubah").innerHTML = `<p> error : ${err.message} </p>`
   }
 }
 
-const fungsi = ({}) => {
-  
+const fungsi = ({first,last, email,city,country,large}) => {
+  document.getElementById("ubah").innerHTML  = `<h4> ${first}, ${last}</h4>
+  <p> ${email}</p>
+  <p> ${city} , ${country}</p>
+  <img src="${large}" alt="gambar">`;
 }
 
 
